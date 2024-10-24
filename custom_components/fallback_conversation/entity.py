fdaf35372502d2599c5e6d9922fe7a6d32d2f4c1
Elias Wernicke <e.w.1111111119@gmail.com>
2024-10-24 14:24:53 +0200

    .

--- custom_components/fallback_conversation/entity.py
+++ custom_components/fallback_conversation/entity.py
@@ -35,16 +35,6 @@ class FallbackResultEntity():
     def extra_state_attributes(self):
         """Return the state attributes."""
         return self._attributes
-    
-    @callback
-    def add_to_platform_start(
-        self,
-        hass: HomeAssistant,
-        platform: EntityPlatform,
-        parallel_updates: asyncio.Semaphore | None,
-    ) -> None:
-        """Start adding an entity to a platform."""
-        super().add_to_platform_start(hass, platform, parallel_updates)
 
     async def async_added_to_hass(self):
         """Run when entity about to be added."""
